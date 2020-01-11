a01c6f914de6ab63165da6a5c66e94f76ed6b882
Alex Dollard <alexdollard141@gmail.com>
2020-01-10 18:26:39 -0600

    tied inquirer prompts together w/ results pushing into arrays
    also basic HTML template made

--- Develop/app.js
+++ Develop/app.js
@@ -1,4 +1,12 @@
 const inquirer = require("inquirer"); 
+const Employee = require("../Develop/lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+let engineers = [];
+let interns = []; 
+let managers = []; 
 
 
 
@@ -29,9 +37,19 @@ function managerInit() {
             name: "office_number",
             message: "What is your manager's office number?",
         }
-    ])
+    ]).then(data => {
+      let new_manager =  new Manager(data.name, data.id, data.email, data.office_number);
+        managers.push(new_manager);
+        console.log(managers);
+        createNewMember(); 
+
+    }).catch(function(err){
+        console.log(err);
+    })
 };
 
+managerInit(); 
+// console.log(managers)
 
 function engineerPrompt() {
     inquirer.prompt ([
@@ -55,7 +73,15 @@ function engineerPrompt() {
             name: "github",
             message: "What is your engineer's github?"
         }
-    ])
+    ]).then(data => {
+        let new_engineer =  new Engineer(data.name, data.id, data.email, data.github);
+          engineers.push(new_engineer);
+          console.log(engineers);
+          createNewMember(); 
+  
+      }).catch(function(err){
+          console.log(err);
+      })
 }
 
 
@@ -81,7 +107,15 @@ function internPrompt() {
             name: "school",
             message: "What is your intern's school?"
         }
-    ])
+    ]).then(data => {
+        let new_intern =  new Intern(data.name, data.id, data.email, data.school);
+          interns.push(new_intern);
+          console.log(interns);
+          createNewMember(); 
+  
+      }).catch(function(err){
+          console.log(err);
+      })
 }
 
 function createNewMember() {
@@ -92,6 +126,24 @@ function createNewMember() {
             message: "Do you want to add a new team member?",
             choices: ["Add new engineer", "Add new intern", "Done adding team members"]
         }
-    ])
+    ]).then(data => {
+        // const choice = data.choice
+        console.log(data.createNewMember);
+        switch(data.createNewMember) {
+            case "Add new engineer":
+            return engineerPrompt(); 
+
+            case "Add new intern":
+            return internPrompt(); 
+
+            case "Done adding team members":
+            return generateHTML(); 
+
+            default:
+            return console.log("defaulted!")
+        }
+    })
 }
+
+// createNewMember(); 
 
